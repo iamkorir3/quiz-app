e984c2c464397b9727e05f09a3a1753a1c4c2e05
Iamkorir3 <korir5956@gmail.com>
2025-06-26 22:20:24 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialstate = {};
+
+function reducer(state, action) {}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialstate);
+
   useEffect(function () {
     fetch("http://localhost:8000/questions")
-      .then((res) => res.json)
+      .then((res) => res.json())
       .then((data) => console.log(data))
       .catch((err) => console.error("error"));
   }, []);
