512ca43b1f7ec3c69919fba8f0903ae1a6847ab0
Iamkorir3 <korir5956@gmail.com>
2025-06-27 13:04:46 +0300

    displaying questions to he UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import startScreen from "./startScreen";
 import { type } from "@testing-library/user-event/dist/type";
 
 const initialstate = {
@@ -16,27 +19,33 @@ function reducer(state, action) {
         questions: action.payload,
         status: "ready",
       };
+    case "dataFailed":
+      return {
+        ...state,
+        statu: "error",
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialstate);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialstate);
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
-      .catch((err) => console.error("error"));
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
 
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <startScreen />}
       </Main>
     </div>
   );
---
+++ src/startScreen.js
@@ -0,0 +1,11 @@
+function startScreen() {
+  return (
+    <div className="start">
+      <h2>Welcome to React Quiz!</h2>
+      <h3>X questions to test your react mastery</h3>
+      <button>Let's start</button>
+    </div>
+  );
+}
+
+export default startScreen;
