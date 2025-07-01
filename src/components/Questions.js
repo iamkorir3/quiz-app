68c65501337292b2688c7e3a5f003fbd92aa5d74
Iamkorir3 <korir5956@gmail.com>
2025-07-01 17:46:21 +0300

    debugging

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,10 +1,13 @@
 import Option from "./option";
 
 function Questions({ question, dispatch, answer }) {
+  function test() {
+    console.log("reached");
+  }
   return (
     <div>
       <h4>{question.question}</h4>
-      <Option question={question} disaptch={dispatch} answer={answer} />
+      <Option question={question} dispatch={dispatch} answer={answer} />
     </div>
   );
 }
