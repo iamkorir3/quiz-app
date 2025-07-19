1f85ac6c823912b192013cb5eff3de465959ae56
iamkorir3 <korir5956@gmail.com>
2025-07-19 23:47:33 +0300

    temp

---
+++ src/components/temp.js
@@ -0,0 +1,157 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import NextButton from "./NextButton";
+import Questions from "./Questions";
+import Progress from "./Progress";
+import StartScreen from "./startScreen";
+import FinishedScreen from "./FinishedScreen";
+import Timer from "./Timer";
+import Footer from "./Footer";
+
+const SECS_PER_QUIZ = 30;
+const initialstate = {
+  questions: [],
+  status: "loading",
+  index: 0,
+  points: 0,
+  answer: null,
+  highscore: 0,
+  secondsRemaining: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        statu: "error",
+      };
+
+    case "start":
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUIZ,
+      };
+
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + 10
+            : state.points,
+      };
+
+    case "nextQuestion":
+      console.log("reached");
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+
+    case "restart":
+      return {
+        ...initialstate,
+        questions: state.questions,
+        status: "ready",
+      };
+
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
+export default function App() {
+  const [
+    { questions, secondsRemaining, status, index, answer, points, highscore },
+    dispatch,
+  ] = useReducer(reducer, initialstate);
+  const numQuestions = questions.length;
+  const maxPossiblePints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              points={points}
+              index={index}
+              maxPossiblePints={maxPossiblePints}
+              numQuestions={numQuestions}
+              answer={answer}
+            />
+            <Questions
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton
+                dispatch={dispatch}
+                index={index}
+                answer={answer}
+                numQuestions={numQuestions}
+              />
+            </Footer>
+          </>
+        )}
+        {status === "finished" && (
+          <FinishedScreen
+            dispatch={dispatch}
+            points={points}
+            highscore={highscore}
+            maxPossiblePints={maxPossiblePints}
+          />
+        )}
+      </Main>
+    </div>
+  );
+}
